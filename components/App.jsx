b726fa3f9c82f5a68413c7b6ef040fa231d3638c
taegyu leem <closer124@gmail.com>
2017-03-06 09:51:27 -0800

    Update App.jsx
    Remove bad lang.

--- components/App.jsx
+++ components/App.jsx
@@ -5,7 +5,6 @@ class App extends Component {
   render() {
     return <div>
             <h1>!!This is definitely a hot (module reloading) React app !</h1>
-            <p>fuck! it takes for ever!!!</p>
            </div>;
   }
 
